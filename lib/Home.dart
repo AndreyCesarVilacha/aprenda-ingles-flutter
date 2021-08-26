d83643d7845413e2ed6fb62b012e41310e0dd1f3
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2021-08-26 06:16:47 -0300

    Criando o layout

---
+++ lib/Home.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:aprenda_ingles/telas/Bichos.dart';
+import 'package:aprenda_ingles/telas/Numeros.dart';
+import 'package:aprenda_ingles/telas/Vogais.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController= TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("aprenda Inglês"),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(
+              text: "Bichos",
+            ),
+            Tab(
+              text: "Números",
+            ),
+            Tab(
+              text: "Vogais",
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Bichos(),
+          Numeros(),
+          Vogais(),
+        ],
+      ),
+    );
+  }
+}
